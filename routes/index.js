16867b21714172e82fba0870ae464726b879b389
brandon-schabel <brandonschabel1995@gmail.com>
2018-04-27 23:46:46 -0700

    Test basic parsing

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,26 @@ const parse = require('csv-parse')
 const fs = require('fs')
 var inputFile = './boltdata.csv'
 
-var parser = parse({delimiter: ';'}, function(err, data) {
-  console.log(data);
-})
+var parser = parse({delimiter: ','}, function(err, data) {
+  // console.log(data);
 
-console.log(fs.createReadStream(inputFile).pipe(parser))
+  data.forEach(function(line) {
+    console.log(line)
+    // console.log(line[2])
+    /*
+    console.log(line[0])
+    for(var i = 0; i < line.length; i++) {
+      console.log(line[i]);
+
+    }
+    */
+    var batteryCharge = { "charge": line[3] }
+    // console.log(line);
+    console.log(JSON.stringify(batteryCharge));
+  });
+})
+// read the inputFile, feed the contents to the parser
+fs.createReadStream(inputFile).pipe(parser)
 
 // var parser = parse
 
